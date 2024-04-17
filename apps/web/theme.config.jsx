a56302df47558077bfe675330ab8dcd30848cb20
Kev1nzh37 <kev1nzh37@gmail.com>
2024-04-17 10:16:13 +0800

    feat: update logo

--- apps/web/theme.config.jsx
+++ apps/web/theme.config.jsx
@@ -4,7 +4,13 @@ export default {
       titleTemplate: '%s – Artify Waves',
     }
   },
-  logo: <span>Artify Waves</span>,
+  logo: <>
+    <img src="/logo/logo.png" style={{ width: 40, height: 40, marginRight: 8 }} alt="" />
+    <span>Artify Waves</span>
+  </>,
+  head: <>
+    <link rel="icon" href="/logo/logo.svg" type="image/svg+xml"></link>
+  </>,
   project: {
     link: "https://github.com/kev1nzh37/artify-waves",
   },
@@ -12,8 +18,10 @@ export default {
   feedback: {
     content: null,
   },
-  navigation: false,
+  // navigation: false,
   editLink: {
     component: null,
-  }
+  },
+  primaryHue: 1,  // 色调
+  primarySaturation: 91,  // 饱和度
 }
